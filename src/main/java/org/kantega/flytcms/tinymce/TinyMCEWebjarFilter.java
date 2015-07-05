115070774003da844dbfacd53219bcde75cfa5f1
Marvin B. Lillehaug <marvin.lillehaug@gmail.com>
2015-07-05 13:44:50 +0200

    All plugsin seems to be proxied.

--- src/main/java/org/kantega/flytcms/tinymce/TinyMCEWebjarFilter.java
+++ src/main/java/org/kantega/flytcms/tinymce/TinyMCEWebjarFilter.java
@@ -1,17 +1,22 @@
 package org.kantega.flytcms.tinymce;
 
+import org.springframework.web.filter.OncePerRequestFilter;
+
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.*;
 import java.net.URL;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 
-public class TinyMCEWebjarFilter implements Filter {
+public class TinyMCEWebjarFilter extends OncePerRequestFilter implements Filter {
 
+    private final String PATH_PREFIX = "/flytcms/tinymce/";
     private String tinymceVersion;
 
-    @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
+    public TinyMCEWebjarFilter() throws ServletException {
+        super();
         try {
             tinymceVersion = readVersion();
         } catch (IOException e) {
@@ -20,12 +25,37 @@ public class TinyMCEWebjarFilter implements Filter {
     }
 
     @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
         // /webjars/tinymce/tinymce.min.js -> /webjars/tinymce/${version}/tinymce.min.js
+        // /aksess/tinymce/tinymce.min.js -> /webjars/tinymce/${version}/tinymce.min.js
+        String requestURI = request.getRequestURI();
+
+        String substring = requestURI.substring(requestURI.indexOf(PATH_PREFIX) + PATH_PREFIX.length());
+
+        String tinyPath = "/webjars/tinymce/" + tinymceVersion + "/" + substring;
+
+        ServletContext sc = request.getServletContext();
+        InputStream is = sc.getResourceAsStream(tinyPath);
+        if(is == null) {
+            is = sc.getResourceAsStream("/flytcms/tinymce/" + substring);
+        }
+
+        if(is != null){
+            httpServletResponse.setHeader("Content-Type", "application/javascript");
+            writeToStream(is, httpServletResponse.getOutputStream());
+        }
+
     }
 
-    @Override
-    public void destroy() {}
+    private void writeToStream(InputStream is, ServletOutputStream outputStream) throws IOException {
+        try (Reader reader = new BufferedReader(new InputStreamReader(is));
+             OutputStream os = outputStream) {
+            int c = 0;
+            while ((c = reader.read()) != -1) {
+                os.write(c);
+            }
+        }
+    }
 
     private String readVersion() throws IOException {
         URL resource = getClass().getResource("tinymce.version");
---
+++ src/main/java/org/kantega/flytcms/tinymce/TinyMceServletContextIinitializer.java
@@ -0,0 +1,18 @@
+package org.kantega.flytcms.tinymce;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.ServletContainerInitializer;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import java.util.EnumSet;
+import java.util.Set;
+
+public class TinyMceServletContextIinitializer implements ServletContainerInitializer {
+    @Override
+    public void onStartup(Set<Class<?>> c, ServletContext ctx) throws ServletException {
+        EnumSet<DispatcherType> dispatcherTypes = EnumSet.allOf(DispatcherType.class);
+        dispatcherTypes.remove(DispatcherType.INCLUDE);
+        ctx.addFilter("tinymcewebjarfilter", TinyMCEWebjarFilter.class)
+                .addMappingForUrlPatterns(dispatcherTypes, false, "/flytcms/tinymce/*");
+    }
+}
